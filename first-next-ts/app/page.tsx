0a929b360c78edfc9b160f94af6bdb2d258b03bb
ClementSellyPro <clementselly.pro@gmail.com>
2024-10-14 20:17:00 +0400

    Setup states for the first page

--- first-next-ts/app/page.tsx
+++ first-next-ts/app/page.tsx
@@ -1,8 +1,32 @@
+'use client'
+
 import Link from "next/link";
 import Button from "./components/Button";
 import Title from "./components/Title";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  function handleName(e: React.ChangeEvent<HTMLElement>){
+    const target = e.target as HTMLInputElement;
+    setName(target.value);
+    console.log(name)
+  }
+
+  function handleEmail(e: React.ChangeEvent<HTMLElement>){
+    const target = e.target as HTMLInputElement;
+    setEmail(target.value);
+  }
+
+  function handlePhoneNumber(e: React.ChangeEvent<HTMLElement>){
+    const target = e.target as HTMLInputElement;
+    setPhoneNumber(target.value);
+  }
+
   return (
     <div className="relative mx-16 w-full py-5">
       <Title title={'Personal info'}>
@@ -13,17 +37,17 @@ export default function Home() {
 
         <div className="flex flex-col">
           <label htmlFor="nameInput" className="text-blue-900 text-sm font-bold">Name</label>
-          <input className="mt-2 p-3 border rounded-md outline-none focus:border-indigo-700" type="text" name="nameInput" id="nameInput" placeholder='e.g. Stephen King' />
+          <input className="mt-2 p-3 border rounded-md outline-none focus:border-indigo-700" type="text" name="nameInput" id="nameInput" placeholder='e.g. Stephen King' value={name} onChange={(e: React.ChangeEvent<HTMLElement>) => handleName(e)} />
         </div>
         
         <div className="flex flex-col">
           <label htmlFor="mailInput" className="text-blue-900 text-sm font-bold">Email Address</label>
-          <input className="mt-2 p-3 border rounded-md outline-none focus:border-indigo-700" type="text" name="mailInput" id="mailInput" placeholder='e.g. stephenking@lorem.com' />
+          <input className="mt-2 p-3 border rounded-md outline-none focus:border-indigo-700" type="text" name="mailInput" id="mailInput" placeholder='e.g. stephenking@lorem.com' value={email} onChange={(e: React.ChangeEvent<HTMLElement>) => handleEmail(e)} />
         </div>
         
         <div className="flex flex-col">
           <label htmlFor="numberInput" className="text-blue-900 text-sm font-bold">Phone Number</label>
-          <input className="mt-2 p-3 border rounded-md outline-none focus:border-indigo-700" type="text" name="numberInput" id="numberInput" placeholder='e.g. +1 234 567 890' />
+          <input className="mt-2 p-3 border rounded-md outline-none focus:border-indigo-700" type="text" name="numberInput" id="numberInput" placeholder='e.g. +1 234 567 890' value={phoneNumber} onChange={(e: React.ChangeEvent<HTMLElement>) => handlePhoneNumber(e)} />
         </div>
 
         {/* button section */}
