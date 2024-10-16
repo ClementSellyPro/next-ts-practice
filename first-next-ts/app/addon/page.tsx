f48486d1102a4edbf67844e9490272d2babeebd6
ClementSellyPro <clementselly.pro@gmail.com>
2024-10-16 20:33:08 +0400

    Add ons page styled

--- first-next-ts/app/addon/page.tsx
+++ first-next-ts/app/addon/page.tsx
@@ -1,21 +1,33 @@
 import Link from 'next/link'
 import React from 'react'
 import Button from '../components/Button'
+import Title from '../components/Title'
+import AddonCard from '../components/AddonCard'
 
 export default function Addon() {
   return (
     <div className="relative mx-16 w-full py-5">
-        Addon
+
+      <Title title="Pick add-ons" >
+        Add-ons help enhance your gaming experience.
+      </Title>
+
+      {/* add-ons section */}
+      <div className='flex flex-col justify-between items-center h-4/6 mt-5'>
+        <AddonCard title="Online service" price="1">Access to multiplayer games</AddonCard>
+        <AddonCard title="Larger storage" price="2">Extra 1TB of cloud save</AddonCard>
+        <AddonCard title="Customizable profile" price="2">Custom theme on your profile</AddonCard>
+      </div>
         
-        {/* buttons section */}
-        <Link href='/plan' className="absolute left-0 bottom-5 text-blue-900 font-bold hover:text-blue-700">
-            Go Back
-        </Link>
-        <Link href='/summary' className="absolute right-0 bottom-5">
-          <Button>
-            Next Step
-          </Button>
-        </Link>
+      {/* buttons section */}
+      <Link href='/plan' className="absolute left-0 bottom-5 text-blue-900 font-bold hover:text-blue-700">
+          Go Back
+      </Link>
+      <Link href='/summary' className="absolute right-0 bottom-5">
+        <Button>
+          Next Step
+        </Button>
+      </Link>
     </div>
   )
 }
---
+++ first-next-ts/app/components/AddonCard.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import React, { MouseEvent, useState } from 'react'
+
+export default function AddonCard({title, children, price}
+:{
+    title:string, 
+    children:string, 
+    price:string
+}) {
+
+    const [selected, setSelected] = useState();
+
+    function handleSelection(e: MouseEvent<HTMLElement>){
+        const target = e.currentTarget as HTMLElement;
+        console.log(target);
+    }
+
+    return (
+        <div className='flex items-center p-8 border rounded-xl w-full relative hover:bg-slate-50 hover:border-indigo-600 hover:cursor-pointer' onClick={(e: MouseEvent<HTMLElement>) => handleSelection (e)}>
+            <input className='w-8' type="checkbox" />
+            <div className='ml-4'>
+                <h2 className='font-semibold text-blue-900'>{title}</h2>
+                <p className='text-slate-400'>{children}</p>
+            </div>
+            <p className='absolute right-8 text-indigo-600'>+${price}/mo</p>
+        </div>
+    )
+}
